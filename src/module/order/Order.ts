e53dbe7d9c817750e7279b9f9db25ac3a64bd7b7
Mathisgmn <157029434+Mathisgmn@users.noreply.github.com>
2025-12-04 15:48:09 +0100

    Add order creation e2e test

--- src/module/order/Order.ts
+++ src/module/order/Order.ts
@@ -1,3 +1,5 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
 export enum OrderStatus {
     PENDING = 'PENDING',
     CONFIRMED = 'CONFIRMED',
@@ -6,19 +8,24 @@ export enum OrderStatus {
     CANCELLED = 'CANCELLED'
 }
 
+@Entity()
 export class Order {
+    @PrimaryGeneratedColumn()
     public id: number;
 
+    @Column('simple-array')
     public productIds: number[];
 
+    @Column({ type: 'float' })
     public totalPrice: number;
 
+    @CreateDateColumn({ type: 'timestamptz' })
     public createdAt: Date;
 
+    @Column({ type: 'enum', enum: OrderStatus, default: OrderStatus.PENDING })
     public status: OrderStatus;
 
     constructor(productsIds: number[], totalPrice: number) {
-
         if (productsIds.length < 1 || productsIds.length > 5) {
             throw new Error('An order must contain between 1 and 5 products.');
         }
@@ -27,11 +34,8 @@ export class Order {
             throw new Error('Total price must be between 2 and 500.');
         }
 
-        this.createdAt = new Date();
         this.status = OrderStatus.PENDING;
-
         this.totalPrice = totalPrice;
         this.productIds = productsIds;
     }
-
 }
